3a1aaa5959a754410e4a815653c448cd8ec7ce2d
Tolkyn Kuanyshkyzy <sdu.tolkyn@gmail.com>
2021-03-29 16:08:13 +0600

    Added Singleton method

--- src/test/java/TestWebDriver.java
+++ src/test/java/TestWebDriver.java
@@ -1,4 +1,3 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.opera.OperaDriver;
@@ -6,18 +5,13 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class TestWebDriver {
     public static void main(String[] args) throws InterruptedException {
-       WebDriver driver = new ChromeDriver();
-       driver.get("https://www.google.com/");
-       Thread.sleep(20000);
-       driver.quit();
+        WebDriver driver = new ChromeDriver();
+        SingletonClass.getDriver(driver);
 
         WebDriver driverSafari = new SafariDriver();
-        driverSafari.navigate().to("http://www.google.com/");
-        Thread.sleep(30000);
-        driverSafari.close();
+        SingletonClass.getDriver(driverSafari);
 
         WebDriver operaDriver = new OperaDriver();
-        operaDriver.navigate().to("http://www.vk.com");
-        operaDriver.quit();
+        SingletonClass.getDriver(operaDriver);
     }
 }
